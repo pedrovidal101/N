839546f594616c8977ae7c02c05ecea6eefba720
root <root@VIDAL_MANCO.localdomain>
2023-08-18 20:26:23 -0500

    mi primer archivo

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+
+def   sun(a,b):
+    result = a+b
+    print (result)
+    return result
+    
+    sen_set = {}
+    sun_set.add(sun(1,2))
+    sun_set.add(sun(3,4))
+
+print("hola  desde mi maquina")
+
+
