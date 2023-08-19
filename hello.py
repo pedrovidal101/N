7647047d86a996b508248928f3afb64f5ad7d7fd
root <root@VIDAL_MANCO.localdomain>
2023-08-18 20:53:27 -0500

    add gitignore

--- hello.py
+++ hello.py
@@ -1,14 +1,5 @@
 
-
-def   sun(a,b):
-    result = a+b
-    print (result)
-    return result
-    
-    sen_set = {}
-    sun_set.add(sun(1,2))
-    sun_set.add(sun(3,4))
-
 print("hola  desde mi maquina")
+
 
 
